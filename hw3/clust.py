f39bdaa124f6857ad8ea268e80d60133905daf34
Willie Yao <wyao09@gmail.com>
2013-03-09 03:20:41 -0500

    K-Means cluster implemented.

--- hw3/clust.py
+++ hw3/clust.py
@@ -1,9 +1,12 @@
 # clust.py
 # -------
-# YOUR NAME HERE
+# Irene Chen and Willie Yao
 
 import sys
 import random
+from utils import *
+import operator
+import py.test
 
 DATAFILE = "adults.txt"
 
@@ -55,8 +58,8 @@ def main():
         print "Usage: clust numClusters numExamples"
         sys.exit(1);
 
-    numClusters = int(sys.argv[1])
-    numExamples = int(sys.argv[2])
+    numClusters = int(sys.argv[1]) #K
+    numExamples = int(sys.argv[2]) #n
 
     #Initialize the random seed
     
@@ -74,11 +77,66 @@ def main():
     
     
     dataset.close()
-    printOutput(data,numExamples)
+    #printOutput(data,numExamples)
+    assert(numExamples <= len(data))
+    assert(numClusters <= numExamples)
 
     # ==================== #
     # WRITE YOUR CODE HERE #
     # ==================== #
+    #py.test.set_trace()
+    # K-means
+    dimension = len(data[0])
+    chosen = set()
+    prototypes = []
+    assignments = [] # Aka responsibility vectors
+
+    # Initialize prototype vectors
+    for _ in xrange(numClusters):
+        r = None
+        while True:
+            r = random.randint(0, numExamples-1)
+            if r not in chosen:
+                chosen.add(r)
+                break
+        prototypes.append(list(data[r]))
+
+    # Repeatedly update responsibility and prototype vectors
+    converged = False
+    iteration = 1
+    while not converged:
+        print iteration
+        # Update responsibility vectors
+        newAssignments = []
+        for n in xrange(numExamples):
+            bestProto = None
+            minDistance = float('inf')
+            for k in xrange(numClusters):
+                d = squareDistance(data[n], prototypes[k])
+                if d < minDistance:
+                    bestProto = k
+                    minDistance = d
+            newAssignments.append(bestProto)
+        if newAssignments == assignments:
+            converged = True
+        assignments = newAssignments
+
+        # Update prototype vectors
+        for k in xrange(numClusters):
+            prototypes[k] = [0.]*dimension
+        counts = [0]*numClusters
+        for n in xrange(numExamples):
+            k = assignments[n]
+            counts[k] += 1
+            prototypes[k] = map(operator.add, prototypes[k], data[n])
+        for k in xrange(numClusters):
+            prototypes[k] = map(lambda x: x/counts[k], prototypes[k])
+
+        iteration += 1
+        err = 0.
+        for n in xrange(numExamples):
+            err += squareDistance(data[n], prototypes[assignments[n]])
+        print err
 
 if __name__ == "__main__":
     validateInput()
