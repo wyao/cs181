b73c3aa110408b8ad287e10335ec849480cf4cfb
Willie Yao <wyao09@gmail.com>
2013-05-05 21:30:23 -0400

    generalize parse.py

--- project/code/parse.py
+++ project/code/parse.py
@@ -1,13 +1,21 @@
+import sys
+
+# python parse.py q_data/2000.p; python parse.py q_data/5000.p; python parse.py q_data/10000.p; python parse.py q_data/20000.p
+# 1 0.245454545455
+# 1 0.177777777778
+# 1 0.30101010101
+# 1 0.384848484848
+
 l = []
 
-f = open("typescript")
+f = open(sys.argv[1])
 for line in f:
     if line.find("Player 1") > -1:
         l.append(1)
     elif line.find("Player 2") > -1:
         l.append(2)
 
-x = 1000
+x = 990
 win = 0.
 for i in xrange(1, len(l)+1):
     if l[i-1] == 1:
